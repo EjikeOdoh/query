5ff9a6d32d351636739d33a4824eb5534d244b7d
EjikeOdoh <ejike.developer@gmail.com>
2025-07-29 06:04:05 +0100

    Update student detail page

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,14 +1,19 @@
 
 type RowProps = {
     label: string
-    value: string
+    value?: string | null,
 }
 
 export default function Row({ label, value }: RowProps) {
     return (
         <div className="flex items-center gap-14">
-            <p className="w-24 font-light text-sm text-[#808080]">{label}</p>
-            <p className="flex-1 font-light text-sm text-[#171717]">{value}</p>
+            <p className="w-28 font-light text-sm text-[#808080]">{label}</p>
+            {
+                !!value ? 
+                <p className="flex-1 font-light text-sm text-[#171717]">{value}</p>
+                : <span className="flex-1 font-light text-sm text-[#171717] italic">Not provided</span>
+            }
+         
         </div>
     )
 }
--- src/pages/StudentDetails.tsx
+++ src/pages/StudentDetails.tsx
@@ -1,7 +1,10 @@
+import Header from "@/components/Header";
 import Heading from "@/components/Heading";
 import Row from "@/components/Row";
+import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { useGetStudentDetails } from "@/hooks/use-students";
-import type { StudentDetail } from "@/utils/types";
+import { Pencil, Trash2 } from "lucide-react";
 import { useParams } from "react-router";
 
 export default function Student() {
@@ -26,14 +29,22 @@ export default function Student() {
         return <div>No student data found</div>;
     }
 
-    const date = new Date(data.dob);
+    console.log(data)
+
+    const { firstName, lastName, class: currentClass, address, school, dob, phone, country, yearJoined, fatherFirstName, fatherLastName, motherFirstName, fatherPhone, motherPhone, favSubject, difficultSubject, email, grades, participations } = data
 
     return (
         <div>
-            <div>
-                <div className="">
-                    <h1>{data.firstName} {data.lastName}</h1>
-                    <div className="subtitle">{data.school}</div>
+            <Header />
+            <div className="p-5">
+                <div className="flex items-center gap-4">
+                    <div className="w-16 h-16 rounded-full flex items-center justify-center bg-[#B0E6FF] border-2 border-[#D9F3FF]">
+                        <p className="text-3xl font-semibold text-[#008BCC]">{firstName[0]}{lastName[0]}</p>
+                    </div>
+                    <div>
+                        <h1 className="font-bold text-lg text-black">{firstName} {lastName}</h1>
+                        <small>Since {yearJoined}</small>
+                    </div>
                 </div>
 
                 <div className="profile-section">
@@ -45,27 +56,159 @@ export default function Student() {
 
                             <Row
                                 label="Date of Birth"
-                                value="Feb 6"
+                                value={String(dob)}
                             />
+
                             <Row
                                 label="School"
-                                value={data.school}
+                                value={school}
                             />
- <Row
-                                label="Phone"
-                                value={data.phone}
+
+                            <Row
+                                label="Class"
+                                value={currentClass}
                             />
-                            <p className="">Phone:</p>
-                            <p className="">{data.phone}</p>
 
-                            <p className="">Country:</p>
-                            <p className="">{data.country}</p>
+                            {
+                                !!(favSubject) && <Row label="Favorite Subject" value={favSubject} />
+                            }
 
-                            <p className="">Year Joined:</p>
-                            <p className="">{data.yearJoined}</p>
+                            {
+                                !!(difficultSubject) && <Row label="Most Difficult Subject" value={difficultSubject} />
+                            }
+
                         </div>
                     </div>
+
+                    <div className="section">
+                        <Heading
+                            text="Parents Details"
+                        />
+                        <div className="info-grid">
+
+                            <Row
+                                label="Father's Name"
+                                value={fatherLastName}
+                            />
+
+                            <Row
+                                label="Mother's Name"
+                                value={motherFirstName}
+                            />
+                        </div>
+                    </div>
+
+
+                    <div className="section">
+                        <Heading
+                            text="Contacts"
+                        />
+                        <div className="info-grid">
+
+                            <Row
+                                label="Email Address"
+                                value={email}
+                            />
+
+                            <Row
+                                label="Phone Number"
+                                value={phone}
+                            />
+
+
+                            <Row
+                                label="House Address"
+                                value={address}
+                            />
+                        </div>
+                    </div>
+
                 </div>
+            </div>
+
+            <div className="p-5">
+                <h3>Grades</h3>
+
+                <Table className="rounded-xl overflow-hidden">
+                    <TableHeader className="">
+                        <TableRow className="bg-[#E6F7FF]">
+                            <TableHead className="text-[#808080] text-sm font-light">Year</TableHead>
+                            <TableHead className="text-[#808080] text-sm font-light min-w-10">Mth</TableHead>
+                            <TableHead className="text-[#808080] text-sm font-light min-w-10">Eng</TableHead>
+                            <TableHead className="text-[#808080] text-sm font-light min-w-10">Chm</TableHead>
+                            <TableHead className="text-[#808080] text-sm font-light min-w-10">Phy</TableHead>
+                            <TableHead className="text-[#808080] text-sm font-light min-w-10">Bio</TableHead>
+                            <TableHead className="text-[#808080] text-sm font-light min-w-10">Gov</TableHead>
+                            <TableHead className="text-[#808080] text-sm font-light min-w-10">Eco</TableHead>
+                            <TableHead className="text-[#808080] text-sm font-light min-w-10">Lit</TableHead>
+                            <TableHead className="text-[#808080] text-sm font-light min-w-10">Act</TableHead>
+                            <TableHead className="text-[#808080] text-sm font-light min-w-10">Com</TableHead>
+                            <TableHead className="text-[#808080] text-sm font-light w-28">Actions</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody >
+                        {grades.map(grade => (
+                            <TableRow key={grade.id}>
+                                <TableCell className="text-[#171717] text-sm font-light">{grade.year}</TableCell>
+                                <TableCell className="text-[#171717] text-sm font-light">{grade.math}</TableCell>
+                                <TableCell className="text-[#171717] text-sm font-light">{grade.english}</TableCell>
+                                <TableCell className="text-[#171717] text-sm font-light">{grade.chemistry}</TableCell>
+                                <TableCell className="text-[#171717] text-sm font-light">{grade.physics}</TableCell>
+                                <TableCell className="text-[#171717] text-sm font-light">{grade.biology}</TableCell>
+                                <TableCell className="text-[#171717] text-sm font-light">{grade.government}</TableCell>
+                                <TableCell className="text-[#171717] text-sm font-light">{grade.economics}</TableCell>
+                                <TableCell className="text-[#171717] text-sm font-light">{grade.literature}</TableCell>
+                                <TableCell className="text-[#171717] text-sm font-light">{grade.accounting}</TableCell>
+                                <TableCell className="text-[#171717] text-sm font-light">{grade.commerce}</TableCell>
+                                <TableCell className="flex items-center justify-center gap-2">
+
+                                    <Button variant="ghost" size="icon">
+                                        <Pencil color="#171717" />
+                                    </Button>
+                                    <Button variant="ghost" size="icon">
+                                        <Trash2 color="#171717" />
+                                    </Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+
+            </div>
+
+
+            <div className="p-5">
+                <h3>Participations</h3>
+
+                <Table className="rounded-xl overflow-hidden">
+                    <TableHeader className="">
+                        <TableRow className="bg-[#E6F7FF]">
+                            <TableHead className="text-[#808080] text-sm font-light min-w-28">Program</TableHead>
+                            <TableHead className="text-[#808080] text-sm font-light min-w-28">Year</TableHead>
+                            <TableHead className="text-[#808080] text-sm font-light min-w-28">Quarter</TableHead>
+                            <TableHead className="text-[#808080] text-sm font-light min-w-28 w-28">Actions</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody >
+                        {participations.map(particpation => (
+                            <TableRow key={particpation.participation_id}>
+                                <TableCell className="text-[#171717] text-sm font-light">{particpation.program_program}</TableCell>
+                                <TableCell className="text-[#171717] text-sm font-light">{particpation.participation_year}</TableCell>
+                                <TableCell className="text-[#171717] text-sm font-light">{particpation.participation_quarter}</TableCell>
+                                <TableCell className="flex items-center justify-center gap-2">
+
+                                    <Button variant="ghost" size="icon">
+                                        <Pencil color="#171717" />
+                                    </Button>
+                                    <Button variant="ghost" size="icon">
+                                        <Trash2 color="#171717" />
+                                    </Button>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+
             </div>
         </div>
     );
