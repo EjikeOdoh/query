ede993b11d240e230d9c6458d890d2f563394ff9
EjikeOdoh <ejike.developer@gmail.com>
2025-11-23 20:20:54 +0100

    Fix salutation on mobile view

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { useSidebar } from "./ui/sidebar";
 import { useEffect, useState } from "react";
 
 export default function Header({ label }: { label?: string }) {
-    const { open } = useSidebar()
+    const { open, isMobile } = useSidebar()
     const [profile, setProfile] = useState<ProfileState>()
 
     useEffect(() => {
@@ -15,7 +15,7 @@ export default function Header({ label }: { label?: string }) {
     return (
         <div className="pb-20 min-w-full flex mb-10 bg-[#F5F5F5] border-b-2 border-white z-[5]">
             <div
-                className={`fixed top-0 h-[80px] bg-[#F5F5F5] px-6 flex items-center justify-between ${open ? "mr-64 w-[calc(100%-256px)]" : "w-[calc(100%-64px)]"
+                className={`fixed top-0 h-[80px] bg-[#F5F5F5] px-6 flex items-center justify-between ${!isMobile && (open) ? "mr-64 w-[calc(100%-256px)]" : isMobile ? "w-full":"w-[calc(100%-64px)]"
                     }`}
             >
 
@@ -26,10 +26,7 @@ export default function Header({ label }: { label?: string }) {
                     </h1>
                 </div>
                 <div className="flex items-center gap-2">
-                    {/* <div className="w-12 h-12 rounded-full flex items-center justify-center bg-[#B0E6FF] border-2 border-[#D9F3FF] p-2">
-                        <p className="text-xl font-bold text-[#008BCC]">{initials}</p>
-                    </div> */}
-                    <div className="space-y-[-6px]">
+                    <div className="flex flex-col items-end">
                         <h6 className="text-[#171717] text-lg font-semibold">Welcome, {name}</h6>
                         <small className="text-[#808080] text-[10px]">{profile?.role}</small>
                     </div>
